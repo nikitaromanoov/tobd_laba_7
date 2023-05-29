0506bed9723cf0feff6d7b191334dacf2a60ef27
nikitaromanoov <nikitaromanoov@gmail.com>
2023-05-29 19:00:41 +0300

    Update all files

--- src/train.py
+++ src/train.py
@@ -1,5 +1,13 @@
-from preprocessing import Preprocess
 from pyspark.ml.clustering import KMeans
+from pyspark.ml.evaluation import ClusteringEvaluator
+from pyspark.ml.feature import VectorAssembler, MinMaxScaler
+from pyspark.sql import SparkSession
+#from pyspark.sql.functions import isnull, when, count, col, lit
+from pyspark.sql.functions import col
+import psutil
+
+
+
 
 import configparser
 
@@ -7,15 +15,45 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 
-def get_params():
-	with open()
 
 
-def train():
-	trainer = KMeans().setK(config["parameters"]["k"]).setSeed(config["parameters"]["seed"])
+class model_KMean():
+    def __init__(self, config):
+        self.spark = SparkSession.builder.appName("test").getOrCreate()
+        self.model = KMeans().setK(int(config["parameters"]["k_p"])).setSeed(int(config["parameters"]["seed"]))
+    def preprocess(self, path):
+        data = self.spark.read.format("csv").option("header", True).option("sep", "\t").load(path)
+        persent_of_data = psutil.virtual_memory().total / (1024.0 **3) * 300000 / data.count()
+        data = data.sample(persent_of_data,1)
+        data = data.select([col(x).cast("float") for x in [ i for i in data.columns if "100g" in i]])
+        data = data.na.fill(0.0).na.fill("unk")
+        data = VectorAssembler(inputCols=data.columns, outputCol="prefeatures").setHandleInvalid("error").transform(data)
+        sc = MinMaxScaler().setInputCol("prefeatures").setOutputCol("features")
+        self.data = sc.fit(data).transform(data)
+        
+        return persent_of_data
+
+    def train(self):
+        metric = ClusteringEvaluator()
+        self.model = self.model.fit(self.data)
+        pred = self.model.transform(self.data)
+        
+        return metric.evaluate(pred)
+        
+    def save(self, path):
+        self.model.save(path)
+
+	
 
 def main():
-	
+    global config
+    model = model_KMean(config)
+    print("_____")
+    print("Size of dataset after preprocessing based on capacity of your device", model.preprocess("./data/en.openfoodfacts.org.products.csv"))
+    print("_____")
+    print("Result:", model.train())
+    print("_____")
+    model.save("model.pth")
 
 if __name__ == "__main__":
 	main()
